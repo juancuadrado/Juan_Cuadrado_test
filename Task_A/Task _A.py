1540327cffe048a6800fbedd17eeabbb8f80e1bb
juancuadrado <73563709+juancuadrado@users.noreply.github.com>
2022-12-05 23:54:07 -0500

    Update Task _A.py

--- Task_A/Task _A.py
+++ Task_A/Task _A.py
@@ -5,16 +5,33 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np  
 
+"""
+This code accepts two lines (x1,x2) and (x3,x4) on 
+the x-axis and returns whether they overlap. 
+As an example, (1,5) and (2,6) overlaps but not (1,5) and (6,8).
+"""
+
+#Line coordinates
 X1 = 5
 X2 = 15
 X3 = 10 
 X4 = 25
 
+"""
+Here the coordinate will be sorted to determine the start and end of each line.
+"""
+#Line coordinates Sorted
 Cord = sorted([X1, X2, X3 ,X4])
 Cor_1 =sorted([X1, X2])
 Cor_2 =sorted([X3, X4])
 
 #Method 1 
+"""
+In this method, the code will create two lines taking into account the spatial spaces 
+and then assigning ones where the lines exist and zeros where the lines do not exist. 
+If the lines overlap in a vector index, thus the value of this vector index will be 2.
+This code can be expanded for multiple lines in the x-axis coordinates.
+"""
 st = time.time()
 All_Line = np.array(range(Cord[0],Cord[3]+1))
 L1  = np.zeros(np.size(All_Line))
@@ -32,6 +49,13 @@ print('Execution time Method 1:', et-st, 'seconds')
 del L1, L2, All_Line, Final
 
 #Method 2
+"""
+ In this method, the code will create two lists. These two lists will contain the line 
+ of the coordinates previously inputted. Then will cast has a set to check if any item 
+ in the first line is contained in the second line. If the first line contains any value 
+ to the other line, the code will print an overlap. This code is more straightforward than
+ the previous one. However, in case that is more than one line, it needs to be rewritten. 
+"""
 st1 = time.time()
 
 L1 = list(range(Cor_1[0],Cor_1[1]+1))
